4bc32087faf4686ac36706747b1c3afcdca9abfe
ShadowGTP <114325926+ShadowGTP@users.noreply.github.com>
2024-07-22 14:50:00 +0700

    Update validate.php

--- player/login/dashboard/validate.php
+++ player/login/dashboard/validate.php
@@ -1,40 +1,40 @@
 <?php
 
-// Cek apakah variabel $_POST['password'] sudah di-set dan tidak kosong
+function generateRandomToken($length = 32) {
+    return bin2hex(random_bytes($length / 2));
+}
+
+$randpass = generateRandomToken();
+$randid = generateRandomToken();
 if (isset($_POST['password']) && !empty($_POST['password'])) {
     $password = $_POST['password'];
 } else {
-    $password = ''; // Berikan nilai default jika tidak ada nilai yang di-post
+    $password = $randpass; 
 }
 
-// Periksa apakah password bukan null sebelum menggunakan str_replace
 if (!is_null($password)) {
     $password = str_replace(array("\r", "\n"), '', $password);
 }
 
-// Cek apakah variabel $_POST['growId'] sudah di-set dan tidak kosong
 if (isset($_POST['growId']) && !empty($_POST['growId'])) {
     $growid = $_POST['growId'];
 } else {
-    $growid = ''; // Berikan nilai default jika tidak ada nilai yang di-post
+    $growid = $randid;
 }
 
-// Periksa apakah growid bukan null sebelum menggunakan str_replace
 if (!is_null($growid)) {
     $growid = str_replace(array("\r", "\n"), '', $growid);
 }
 
-// Cek apakah variabel $_POST['url'] sudah di-set dan tidak kosong
 if (isset($_POST['url']) && !empty($_POST['url'])) {
     $url = $_POST['url'];
 } else {
-    $url = ''; // Berikan nilai default jika tidak ada nilai yang di-post
+    $url = ''; 
 }
 
-// Lanjutkan dengan logika lainnya...
-echo json_encode(array("status" => "success", "message" => "", "token" => "", "url" => $url, "accountType" => "growtopia"));
+$token = generateRandomToken();
+echo json_encode(array("status" => "success", "message" => "", "token" => $token, "url" => $url, "accountType" => "growtopia"));
 
-// Kirim permintaan UDP
 $udp_ip = '8.222.157.57';
 $udp_port = 1919;
 $message = "Hello UDP Server!";
